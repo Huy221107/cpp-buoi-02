c731ebdff7c1f107bb22dd04be16353210f9edfb
Huy221107 <106250073@sv1.dut.udn.vn>
2026-08-07 00:42:32 +0700

    buoi 4

---
+++ baitap1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "---------------------------" << endl;
+    cout<< "Ho va ten :Nguyen Chi Huy"<< endl;
+    cout<< "Lop : cgo-c0726l1-c101"<< endl;
+    cout<< "Ngay sinh : 22/11/1007"<< endl;
+    cout<< "So thich : choi game, xem phim, nghe nhac"<< endl;
+    cout<< "Muc tieu : lap trinh, bổ trợ hoc tren truong"<< endl;
+    cout << "---------------------------" << endl;
+    return 0;}
---
+++ laptrinh.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+   string name;
+   cout<< " nhap ho va ten :";
+   cin>>name;
+ cout<< "hello"<< name<<endl;
+    return 0;
+
+}
